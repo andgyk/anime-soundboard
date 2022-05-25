7e6fa072f0ab589056641d7e7b5985262936ea6a
andgyk <an4g7k@gmail.com>
2022-05-25 20:31:02 +0600

    Deploy gh-pages

---
+++ out/_next/static/41FpCiY3NF8gHcvdeoxfQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-97a0bae40744ca8d.js"],"/404":["static/chunks/pages/404-f9f3c6ec0209b043.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
