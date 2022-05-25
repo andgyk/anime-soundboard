fdbc77caa9b722086f6714f8ca75c140fcaa7db0
andgyk <an4g7k@gmail.com>
2022-05-25 23:36:05 +0600

    add "nani" sound effect

--- components/audio-component.js
+++ components/audio-component.js
@@ -3,6 +3,7 @@ export default function AudioComponent () {
         <>
         <audio src="./audio/ara-ara.mp3" id="ara-ara-audio"></audio>
         <audio src="./audio/hashira-konichiwa.mp3" id="hasira-konichiwa-audio"></audio>
+        <audio src="./audio/nani.mp3" id="nani-audio"></audio>
         <audio src="./audio/rikka-ow.mp3" id="rikka-ow-audio"></audio>
         <audio src="./audio/yemete-kudasai.mp3" id="yemete-audio"></audio>
         <audio src="./audio/yemete-kudasai-ah.mp3" id="yemete2-audio"></audio>
